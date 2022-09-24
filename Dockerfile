fd021aefda7e8c3b0d5e2c588cb85beb58a51fda
M0L0D0Y <i7ascha@gmail.com>
2022-09-24 19:08:06 +0300

    feat: докер собирает образы и запускает контейнеры

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM amazoncorretto:11-alpine-jdk
-COPY target/*.jar app.jar
+COPY server/target/*.jar app.jar
+COPY gateway/target/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
@@ -18,7 +18,7 @@ public class BookingClient extends BaseClient {
     private static final String API_PREFIX = "/bookings";
 
     @Autowired
-    public BookingClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
+    public BookingClient(@Value("http://localhost:9090") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
@@ -27,21 +27,6 @@ public class BookingClient extends BaseClient {
         );
     }
 
-    /*
-        public ResponseEntity<Object> getBookings(long userId, BookingState state, Integer from, Integer size) {
-            Map<String, Object> parameters = Map.of(
-                    "state", state.name(),
-                    "from", from,
-                    "size", size
-            );
-            return get("?state={state}&from={from}&size={size}", userId, parameters);
-        }
-
-
-        public ResponseEntity<Object> bookItem(long userId, BookItemRequestDto requestDto) {
-            return post("", userId, requestDto);
-        }
-    */
     public ResponseEntity<Object> getBooking(long userId, Long bookingId) {
         return get("/" + bookingId, userId);
     }
